137ba4f4a4d0656e6a740d73d248f7714de9128a
Dhwani-Patel <60955177+Dhwani-Patel@users.noreply.github.com>
2020-02-23 21:36:27 +0530

    delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var color1=document.querySelector(".color1");
-var color2=document.querySelector(".color2");
-
-var body=document.getElementById("body_gradient");
-function setGreadient()
-{
-		body.style.background="linear-gradient(to right,"+color1.value+","+color2.value+")";
-}
